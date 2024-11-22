f42249e578dbcd63634c5aecdb3bd17596d9627f
zowhere <zozobulle17@gmail.com>
2024-11-23 01:27:06 +0200

    define rock, paper,scissor

--- theGame.cpp
+++ theGame.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 #include <cstdlib> //includes random integer
+#include <ctime>
+
 // rock  beats scissor
 // scissor beats paper
 // paper beats rock
 // everything else is a draw
+
+#define ROCK 1
+#define PAPER 2
+#define SCISSOR 3
 
 int main()
 
