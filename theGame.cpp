9ac19782d798e5e37824eca131f848af56b9f8b0
zowhere <zozobulle17@gmail.com>
2024-11-23 01:23:16 +0200

    setting up game
    got the libraries we'll need and set up main

---
+++ theGame.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib> //includes random integer
+// rock  beats scissor
+// scissor beats paper
+// paper beats rock
+// everything else is a draw
+
+int main()
+
+{
+    return 0;
+}
